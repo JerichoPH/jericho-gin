4e60e7ce60591f65f206d0c2d220d3d71964a242
jericho <jericho_ph@qq.com>
2023-12-08 00:24:41 +0800

    修改æ¨用户模型不可序列化密码

--- models/account.mdl.go
+++ models/account.mdl.go
@@ -5,7 +5,7 @@ type (
 	AccountModel struct {
 		MySqlModel
 		Username  string           `gorm:"unique;type:varchar(64);not null;comment:账号;" json:"username"`
-		Password  string           `gorm:"type:varchar(255);not null;comment:密码;" json:"password"`
+		Password  string           `->;gorm:"type:varchar(255);not null;comment:密码;" json:"password"`
 		Nickname  string           `gorm:"unique;type:varchar(64);not null;comment:昵称;" json:"nickname"`
 		RbacRoles []*RbacRoleModel `gorm:"many2many:pivot_rbac_roles__accounts;foreignKey:uuid;joinForeignKey:account_uuid;references:uuid;joinReferences:rbac_role_uuid;" json:"rbacRoles"`
 	}
